95da0a8be97b8ae6e269a34f2111a9a75214eaa9
Sarah Wood <wood.sarah@outlook.com>
2020-10-07 11:54:15 +0100

    Update

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -28,7 +28,7 @@ class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).full(null),
+      squares: Array(9).fill(null),
     };
   }
   renderSquare(i) {
