8c8160b45ac3e1b01fe45e70ab091207387e870e
Sarah Wood <wood.sarah@outlook.com>
2020-10-08 22:25:53 +0100

    Update

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -87,8 +87,8 @@ class Game extends React.Component {
         </div>
       </div>
     );
-  }
-}
+  }}
+
 
 ReactDOM.render(
   <Game />,
@@ -102,16 +102,15 @@ function calculateWinner(squares) {
     [6, 7, 8],
     [0, 3, 6],
     [1, 4, 7],
-    [2, 4, 8],
+    [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[a] === squares[c]) {
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
   }
-
   return null;
 }
